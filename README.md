97e96b753b67fbf8904aff723c5ca32cf4ebc6ba
Adam Hintz <adamh.zero@gmail.com>
2019-02-23 16:36:47 -0800

    Add script to make it super easy to get up-and-running with a new Ubuntu-based machine

---
+++ README.md
@@ -0,0 +1,5 @@
+Script to symlink all my dotfiles and install necessary software:
+
+```
+wget -qO- https://raw.githubusercontent.com/adam000/dotfiles/master/setup/ubuntu.bash | bash
+```
---
+++ setup/ubuntu.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Fail at the first command
+set -e
+# Fail if the first thing in the pipe fails
+set -o pipefail
+# Don't allow unset variables
+set -u
+
+# Clone dotfiles
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y git
+mkdir ~/src
+cd ~/src
+git clone --depth=1 https://github.com/adam000/dotfiles.git
+cd ..
+
+# zsh
+sudo apt-get install -y zsh
+ln -s src/dotfiles/zshenv .zshenv
+mkdir .zsh
+cd .zsh
+ln -s ../src/dotfiles/zsh/zshrc .zshrc
+ln -s ../src/dotfiles/zsh/zshenv .zshenv
+ln -s ../src/dotfiles/zsh/zlogin .zlogin
+cd ..
+
+# tmux
+sudo apt-get install -y tmux
+ln -s src/dotfiles/zsh/tmux.conf .tmux.conf
+mkdir .tmux
+cd .tmux
+ln -s ../src/dotfiles/3grid 3grid
+ln -s ../src/dotfiles/autocomplete autocomplete
+ln -s ../src/dotfiles/default default
+cd ..
+
+# vim
+sudo apt-get install -y vim
+ln -s src/dotfiles/vimrc .vimrc
+
+# Need to change the shell to zsh
+echo "now run: chsh -s \$(which zsh)"
