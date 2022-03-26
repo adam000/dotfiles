84e707b02b058cb35fe57fd4d6470d10fe0bef6a
Adam Hintz <adamh.zero@gmail.com>
2022-03-26 11:02:24 -0700

    Ubuntu setup: two-phase setup which emphasizes using tmux to avoid disconnect problems

--- setup/ubuntu.bash
+++ setup/ubuntu.bash
@@ -9,42 +9,59 @@ set -u
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y git openssh-server htop tree
 
-# Clone dotfiles
-mkdir ~/src
-cd ~/src
-git clone --depth=1 https://github.com/adam000/dotfiles.git
-cd ..
+if ! which tmux; then
+    echo "Bootstrap phase 1/2: intall tmux"
+    # Install tmux, then quit and tell the user to do it again in tmux
+    # (got burned by disconnecting setups one to many times)
+    sudo apt-get install -y tmux
+    ln -s src/dotfiles/tmux.conf .tmux.conf
+    mkdir .tmux
+    cd .tmux
+    ln -s ../src/dotfiles/tmux/3grid 3grid
+    ln -s ../src/dotfiles/tmux/autocomplete autocomplete
+    ln -s ../src/dotfiles/tmux/default default
+    cd ..
+    echo "Please run this script again in tmux (in case there's a disconect)"
+else
+    echo "Bootstrap phase 2/2: install everything else"
+    sudo apt-get install -y git openssh-server htop tree
 
-# zsh
-sudo apt-get install -y zsh
-ln -s src/dotfiles/zshenv .zshenv
-mkdir .zsh
-cd .zsh
-ln -s ../src/dotfiles/zsh/zshrc .zshrc
-ln -s ../src/dotfiles/zsh/zshenv .zshenv
-ln -s ../src/dotfiles/zsh/zlogin .zlogin
-cd ..
+    # Clone dotfiles
+    mkdir ~/src
+    cd ~/src
+    git clone --depth=1 https://github.com/adam000/dotfiles.git
+    cd ..
 
-# tmux
-sudo apt-get install -y tmux
-ln -s src/dotfiles/tmux.conf .tmux.conf
-mkdir .tmux
-cd .tmux
-ln -s ../src/dotfiles/tmux/3grid 3grid
-ln -s ../src/dotfiles/tmux/autocomplete autocomplete
-ln -s ../src/dotfiles/tmux/default default
-cd ..
+    # zsh
+    sudo apt-get install -y zsh
+    ln -s src/dotfiles/zshenv .zshenv
+    mkdir .zsh
+    cd .zsh
+    ln -s ../src/dotfiles/zsh/zshrc .zshrc
+    ln -s ../src/dotfiles/zsh/zshenv .zshenv
+    ln -s ../src/dotfiles/zsh/zlogin .zlogin
+    cd ..
 
-# vim
-sudo apt-get install -y vim
-ln -s src/dotfiles/vimrc .vimrc
+    # tmux
+    sudo apt-get install -y tmux
+    ln -s src/dotfiles/tmux.conf .tmux.conf
+    mkdir .tmux
+    cd .tmux
+    ln -s ../src/dotfiles/tmux/3grid 3grid
+    ln -s ../src/dotfiles/tmux/autocomplete autocomplete
+    ln -s ../src/dotfiles/tmux/default default
+    cd ..
 
-# git
-ln -s src/dotfiles/gitconfig .gitconfig
-ln -s src/dotfiles/gitignore .gitignore
+    # vim
+    sudo apt-get install -y vim
+    ln -s src/dotfiles/vimrc .vimrc
 
-# Need to change the shell to zsh
-echo "Now run: chsh -s \$(which zsh)"
-echo "And run vim once if you want to install all the necessary plugins"
+    # git
+    ln -s src/dotfiles/gitconfig .gitconfig
+    ln -s src/dotfiles/gitignore .gitignore
+
+    # Need to change the shell to zsh
+    echo "Now run: chsh -s \$(which zsh)"
+    echo "And run vim once if you want to install all the necessary plugins"
+fi
